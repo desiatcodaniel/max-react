1ea0cda8d55c1e8b1d69c57e92b94df0c65730e7
desiatcodaniel <desiatcodaniel@gmail.com>
2022-02-21 23:07:57 -0800

    setup eslint for better formatting of react code

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import ExpenseItem from "./components/ExpenseItem";
+import ExpenseItem from './components/ExpenseItem'
 
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
-      <ExpenseItem />
+      <h2>Let&apos;s get started!</h2>
+      <ExpenseItem> </ExpenseItem>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,11 @@
-function ExpenseItem() {
-  return <h2>Date</h2>;
+function ExpenseItem () {
+  return (
+    <div>
+      <div>December 4, 1995</div>
+      <h2>Car Insurance</h2>
+      <div>$1,024.00</div>
+    </div>
+  )
 }
 
-export default ExpenseItem;
+export default ExpenseItem
