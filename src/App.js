6cea97dab8e05b8e22867c09ba1dd5eb451569c1
desiatcodaniel <desiatcodaniel@gmail.com>
2022-02-28 21:04:00 -0800

    remove linter as it was requiring TS, use map to create and display components from an array

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
 import ExpenseItem from './components/ExpenseItem'
 
 function App() {
+  const expenses = [
+    {
+      title: 'Car Insurance',
+      amount: '50.00',
+      date: new Date(2021, 12, 4),
+      id: 0
+    },
+    {
+      title: 'License',
+      amount: '120.00',
+      date: new Date(2021, 12, 6),
+      id: 1
+    },
+    {
+      title: 'Gas',
+      amount: '30.00',
+      date: new Date(2021, 12, 9),
+      id: 2
+    }
+  ]
   return (
     <div>
       <h2>Let&apos;s get started!</h2>
-      <ExpenseItem> </ExpenseItem>
+
+      {
+        //Map all data into ExpenseItem Component
+        expenses.map((item) => {
+          return (
+            <ExpenseItem
+              title={item.title}
+              date={item.date}
+              amount={item.amount}
+              key={item.id}></ExpenseItem>
+          )
+        })
+      }
     </div>
   )
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,14 @@
-function ExpenseItem () {
+import './ExpenseItem.css'
+
+function ExpenseItem(props) {
+  const date = props.date.toLocaleString()
   return (
-    <div>
-      <div>December 4, 1995</div>
-      <h2>Car Insurance</h2>
-      <div>$1,024.00</div>
+    <div className="expense-item">
+      <div>{date}</div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
     </div>
   )
 }
