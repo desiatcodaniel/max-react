2d3be986cf3e5d4685c6c06b35db7e2c3b543730
desiatcodaniel <desiatcodaniel@gmail.com>
2022-04-25 22:04:59 -0700

    fix expense component to encalsulate ALL expense items, not each

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -4,14 +4,18 @@ import ExpenseItem from './ExpenseItem'
 function Expenses(props) {
   const expenses = props.expenses
   return (
-    //Generate Expense items based on the number of given expenses
-    expenses.map((item) => {
-      return (
-        <div className="expenses">
-          <ExpenseItem title={item.title} date={item.date} amount={item.amount} key={item.id} />
-        </div>
-      )
-    })
+    //Map all data into ExpenseItem Component
+    <div className="expenses">
+      {expenses.map((item) => {
+        return (
+          <ExpenseItem
+            title={item.title}
+            date={item.date}
+            amount={item.amount}
+            key={item.id}></ExpenseItem>
+        )
+      })}
+    </div>
   )
 }
 
